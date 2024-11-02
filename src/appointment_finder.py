17ccdd93d543cc94f34c314398be503953769227
Nikita Starkov <lucasudar@gmail.com>
2024-11-02 09:11:16 -0700

    Update .gitignore and enhance appointment finder logic with WebDriverWait

--- src/appointment_finder.py
+++ src/appointment_finder.py
@@ -10,7 +10,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from creds import username, password, facility_name, latest_notification_date, seconds_between_checks
 from telegram import send_message, send_photo
-from urls import BASE_URL, SIGN_IN_URL, SCHEDULE_URL, APPOINTMENTS_URL
+from urls import SIGN_IN_URL, SCHEDULE_URL, APPOINTMENTS_URL
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def log_in(driver):
@@ -19,31 +22,30 @@ def log_in(driver):
 
     print('Logging in.')
 
-    # Clicking the OK button in the "You need to sign in or sign up before continuing" dialog
-    ok_button = driver.find_element(By.XPATH, '/html/body/div[7]/div[3]/div/button')
+    ok_button = driver.find_element(
+        By.XPATH, '/html/body/div[7]/div[3]/div/button')
     if ok_button:
         ok_button.click()
 
-    # Filling the user and password
     user_box = driver.find_element(By.NAME, 'user[email]')
     user_box.send_keys(username)
     password_box = driver.find_element(By.NAME, 'user[password]')
     password_box.send_keys(password)
 
-    # Clicking the checkbox
-    driver.find_element(By.XPATH, '//*[@id="sign_in_form"]/div[3]/label/div').click()
+    driver.find_element(
+        By.XPATH, '//*[@id="sign_in_form"]/div[3]/label/div').click()
 
-    # Clicking 'Sign in'
     driver.find_element(By.XPATH, '//*[@id="sign_in_form"]/p[1]/input').click()
 
-    # Waiting for the page to load.
     time.sleep(2)
     print('Logged in.')
 
 
 def is_worth_notifying(year, month, days):
-    first_available_date_object = datetime.datetime.strptime(f'{year}-{month}-{days[0]}', "%Y-%B-%d")
-    latest_notification_date_object = datetime.datetime.strptime(latest_notification_date, '%Y-%m-%d')
+    first_available_date_object = datetime.datetime.strptime(
+        f'{year}-{month}-{days[0]}', "%Y-%B-%d")
+    latest_notification_date_object = datetime.datetime.strptime(
+        latest_notification_date, '%Y-%m-%d')
 
     return first_available_date_object <= latest_notification_date_object
 
@@ -54,12 +56,13 @@ def check_appointments(driver):
 
     driver.get(APPOINTMENTS_URL)
 
-    # Clicking the Continue button in case of rescheduling multiple people to include all
     continue_button = driver.find_element(By.CLASS_NAME, 'primary')
     if continue_button and continue_button.get_property('value') == 'Continue':
         continue_button.click()
 
-    facility_select = Select(driver.find_element(By.ID, 'appointments_consulate_appointment_facility_id'))
+    facility_select = WebDriverWait(driver, 2).until(EC.presence_of_element_located(
+        (By.ID, 'appointments_consulate_appointment_facility_id')))
+    facility_select = Select(facility_select)
     facility_select.select_by_visible_text(facility_name)
     time.sleep(1)
 
@@ -67,8 +70,9 @@ def check_appointments(driver):
         print("No dates available")
         return
 
-    # Click on "Date of Appointment" to display calendar
-    driver.find_element(By.ID, 'appointments_consulate_appointment_date').click()
+    date_picker = WebDriverWait(driver, 2).until(EC.element_to_be_clickable(
+        (By.ID, 'appointments_consulate_appointment_date')))
+    date_picker.click()
 
     while True:
         for date_picker in driver.find_elements(By.CLASS_NAME, 'ui-datepicker-group'):
@@ -76,27 +80,71 @@ def check_appointments(driver):
             available_days = [day_element.find_element(By.TAG_NAME, 'a').get_attribute("textContent")
                               for day_element in day_elements if day_element.get_attribute("class") == ' undefined']
             if available_days:
-                month = date_picker.find_element(By.CLASS_NAME, 'ui-datepicker-month').get_attribute("textContent")
-                year = date_picker.find_element(By.CLASS_NAME, 'ui-datepicker-year').get_attribute("textContent")
-                message = f'Available days found in {month} {year}: {", ".join(available_days)}. Link: {SIGN_IN_URL}'
+                month = date_picker.find_element(
+                    By.CLASS_NAME, 'ui-datepicker-month').get_attribute("textContent")
+                year = date_picker.find_element(
+                    By.CLASS_NAME, 'ui-datepicker-year').get_attribute("textContent")
+                message = f'Available days found in {month} {year}: {
+                    ", ".join(available_days)}. Link: {SIGN_IN_URL}'
                 print(message)
 
                 if not is_worth_notifying(year, month, available_days):
-                    print("Not worth notifying.")
+                    print(
+                        "Not worth notifying.")
                     return
+
+                for day_element in day_elements:
+                    if day_element.get_attribute("class") == ' undefined':
+                        day_element.find_element(By.TAG_NAME, 'a').click()
+                        break
+
+                time_picker = WebDriverWait(driver, 2).until(EC.element_to_be_clickable(
+                    (By.ID, 'appointments_consulate_appointment_time')))
+                time_picker.click()
+
+                def wait_for_options(driver):
+                    select = Select(driver.find_element(
+                        By.ID, 'appointments_consulate_appointment_time'))
+                    options = select.options
+                    return len(options) > 1 and any(opt.text.strip() for opt in options)
+
+                WebDriverWait(driver, 2).until(wait_for_options)
+
+                time_select = Select(driver.find_element(
+                    By.ID, 'appointments_consulate_appointment_time'))
+                options = time_select.options
+                # print(f"Available options ({len(options)}):")
+                # for i, opt in enumerate(options):
+                #     print(f"{i}: {opt.text}")
+
+                if len(options) > 1:
+                    time_select.select_by_index(1)
+
+                continue_button = WebDriverWait(driver, 2).until(
+                    EC.element_to_be_clickable((By.CLASS_NAME, 'primary'))
+                )
+                continue_button.click()
+
+                confirm_button = WebDriverWait(driver, 2).until(
+                    EC.element_to_be_clickable(
+                        (By.CSS_SELECTOR, 'a.button.alert'))
+                )
+                confirm_button.click()
 
                 send_message(message)
                 send_photo(driver.get_screenshot_as_png())
                 return
 
-        # Skipping two months since we processed both already
         driver.find_element(By.CLASS_NAME, 'ui-datepicker-next').click()
         driver.find_element(By.CLASS_NAME, 'ui-datepicker-next').click()
 
 
 def main():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_experimental_option(
+        "excludeSwitches", ["enable-automation"])
+    # chrome_options.add_argument("--headless")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
